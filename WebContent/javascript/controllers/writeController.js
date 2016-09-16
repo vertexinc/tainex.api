c8cbdc9cdd3beff203c6cf9679d807aa8186636e
Adam Wang <hwang12wou@gmail.com>
2016-09-16 10:07:11 -0400

    doc ref

--- WebContent/javascript/controllers/writeController.js
+++ WebContent/javascript/controllers/writeController.js
@@ -8,13 +8,10 @@ app.controller('writeController', [ '$scope', '$timeout', '$interval',
 
 			$scope.refData = function() {
 				$scope.gridOptions.data.length = 0;
-
-				$timeout(function() {
-					$scope.gridOptions.data = DocArray;
-					
-					$scope.$apply();
-					console.log("refreshed!")
-				});
+				for(var i = 0; i < DocArray.length;i++){
+					$scope.gridOptions.data.push(DocArray[i]);
+				}
+			
 
 				/*
 				 * for(var i = 0; i < DocArray.length; i++){
