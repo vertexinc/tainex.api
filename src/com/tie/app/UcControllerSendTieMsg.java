ebe39b287364e89c21ad276bc2465ddc413e10d4
Adam Wang <hwang12wou@gmail.com>
2017-03-10 16:23:14 -0500

    structure for sending msg

--- src/com/tie/app/UcControllerSendTieMsg.java
+++ src/com/tie/app/UcControllerSendTieMsg.java
@@ -13,32 +13,45 @@ import org.slf4j.LoggerFactory;
 public class UcControllerSendTieMsg extends TieControllerBase {
 	final Logger logger = LoggerFactory.getLogger(TaxDocParser.class);
 	TieSessionController sessionController;
-	private Map<Long,TieMsgPackage> msgPackages;
-	
+	private Map<Long, TieMsgPackage> msgPackages;
+
 	public UcControllerSendTieMsg() {
 
 	}
+
 	public UcControllerSendTieMsg(TieSessionController sessionController) {
 		this.sessionController = sessionController;
 	}
 
-
-	public void sendTieMsg(long msgId) {	
+	public void sendTieMsg(long msgId) {
 		prepareTieMsg(msgId);
-	}
-	
-	//prepare one package for each intended recipient. 
-	public List<TieMsgPackage> prepareTieMsg(long msgId){
+	}//sendTieMsg(.)
+
+	// prepare one package for each intended recipient.
+	public List<TieMsgPackage> prepareTieMsg(long msgId) {
 		logger.debug("Message to be sent with ID {}.", msgId);
 		return null;
-	};
-	
-	//TODO Delegate to the main persister, to read all records,
-	//by invoking individual table persisters and assemble all the 
-	//resulting objects together.  
-	public TieMsg buildTieMsg(long msgId){
-		TieMsg currMsg =TieMainPage.getTieMainPage().getCurrentMsg();
-		currMsg.initPackages();
+	}//prepareTieMsg(.)
+
+	/*TODO Delegate to the main persister, to read all records,
+	 * by invoking individual table persisters and assemble all the
+	 * resulting objects together.
+	 * */
+	public TieMsg buildTieMsg(long msgId) {
+		TieMsg currMsg = TieMainPage.getTieMainPage().getCurrentMsg();
 		return currMsg;
-	}
+	}//end buildTieMsg(.)
+
+	Byte[] packageMsg(TieMsgPackage tieMsgPkg) {
+		return null;
+	}//end packageMsg(.)
+
+	public String sendTaxMsgPackage(TieMsgPackage tieMsgPkg) {
+		return null;
+	}//end sendTaxMsgPackage(.)
+	
+	public boolean recordTaxMsgStatus(){
+		return false;
+	}//end recordTaxMsgStatus()
+
 }
--- src/com/tie/model/TieMsgPackage.java
+++ src/com/tie/model/TieMsgPackage.java
@@ -1,6 +1,22 @@
 package com.tie.model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-//A backpointer towards the tiemsg class
+import com.tie.app.TaxDocParser;
+
 public class TieMsgPackage {
+	TieMsg tiemsg;//A backpointer towards the tiemsg class
+	byte[] packageBytes;//The whole package to be sent
+	String payload;//The message body for the receipient
+	byte[] payloadEncrypted;
+	final Logger logger = LoggerFactory.getLogger(TaxDocParser.class);
+	public TieMsgPackage() {
+		
+	}
+
+	public TieMsgPackage(TieMsg tiemsg) {
+		this.tiemsg = tiemsg;
+		logger.debug("Message in the package {}.", tiemsg.toString());
+	}
 	
-}
+}//end class TieMsgPackage
