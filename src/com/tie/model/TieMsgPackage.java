a27a51b2f107b8234b9716d4d5c2311459785c2c
Adam Wang <hwang12wou@gmail.com>
2017-05-09 13:26:15 -0400

    check remote

--- src/com/tie/model/TieMsgPackage.java
+++ src/com/tie/model/TieMsgPackage.java
@@ -1,17 +1,19 @@
 package com.tie.model;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.tie.app.TaxDocParser;
 
 public class TieMsgPackage {
-	TieMsg tiemsg;//A backpointer towards the tiemsg class
-	byte[] packageBytes;//The whole package to be sent
-	String payload;//The message body for the receipient
+	TieMsg tiemsg;// A backpointer towards the tiemsg class
+	byte[] packageBytes;// The whole package to be sent
+	String payload;// The message body for the receipient
 	byte[] payloadEncrypted;
 	final Logger logger = LoggerFactory.getLogger(TaxDocParser.class);
+
 	public TieMsgPackage() {
-		
+
 	}
 
 	public TieMsgPackage(TieMsg tiemsg) {
@@ -54,5 +56,5 @@ public class TieMsgPackage {
 	public Logger getLogger() {
 		return logger;
 	}
-	
-}//end class TieMsgPackage
+
+}// end class TieMsgPackage
