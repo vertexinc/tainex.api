a6f37dafd93b783c0dfb246aa4c61621449611dd
Adam Wang <hwang12wou@gmail.com>
2017-07-05 10:39:37 -0400

    pwd reset

--- src/com/tie/dao/BaseDao.java
+++ src/com/tie/dao/BaseDao.java
@@ -35,7 +35,7 @@ public class BaseDao {
 			String dbName = "mx";
 			String driver = "com.mysql.jdbc.Driver";
 			String userName = "root";
-			String password = "new-password";
+			String password = "000";
 			if(conn != null){
 				return conn;
 			}else{
